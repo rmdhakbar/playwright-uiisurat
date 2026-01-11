94b372a4a3d93aceeb341d7ae3498a1a09d0a8c6
rmdhakbar <ramadhaniakbar991@gmail.com>
2026-01-11 23:52:34 +0700

    Add login tests with UI validation and credential login functionality

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,8 +3,8 @@ import { HomePage } from "../pages/HomePage";
 import { LoginPage } from "../pages/LoginPage";
 import { ROUTES } from "../config/routes";
 
-const username = process.env.PLAYWRIGHT_USERNAME;
-const password = process.env.PLAYWRIGHT_PASSWORD;
+const username = process.env.PLAYWRIGHT_USERNAME!;
+const password = process.env.PLAYWRIGHT_PASSWORD!;
 let loginPage: LoginPage;
 let homePage: HomePage;
 
@@ -17,6 +17,15 @@ test.describe("Login Test", () => {
   });
   test("Should display login page", async ({ page }) => {
     await expect(page).toHaveURL(ROUTES.LOGIN);
+  });
+  test("Validate Login UI", async ({ page }) => {
     await expect(loginPage.logoUII).toBeVisible();
+    await expect(loginPage.usernameInput).toBeVisible();
+    await expect(loginPage.PasswordInput).toBeVisible();
+    await expect(loginPage.loginButton).toBeVisible();
+  });
+  test("Login with Valid Credential", async ({ page }) => {
+    await loginPage.login(username, password);
+    await expect(page).toHaveURL(ROUTES.HOME);
   });
 });
